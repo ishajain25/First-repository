2723243b37c4f4f8b6bd544a9d3f14e3c98f833e
ISHA <johndoe@example.com>
2021-06-19 16:27:18 +0530

    updates

---
+++ javascript/index.js
@@ -0,0 +1,83 @@
+// Arrays
+// 1-100 
+/*
+var num= [1,2,3,4,5,6,7,38 ,100];
+
+console.log(num[7]);
+
+//Objects
+var obj={
+    name:"isha",
+    age:1500,
+};
+
+console.log(obj)
+
+const classA = [{
+    
+},
+{
+    
+},
+{
+
+}]
+*/
+
+// CONTROL STATEMENTS
+
+/* 
+
+var a=2;
+var b;
+var c=1;
+var d;
+
+b=a++;
+d=++c; 
+
+console.log(b);
+console.log(a);
+console.log(d);
+ */
+
+// FUNCTIONS
+
+//    ASYNCHRONOUS JAVASCRIPT 
+
+//Ajax- asynchronous javascript and xml 
+
+
+// !!!!!!!!! PROMISES !!!!!!!!!!!!1111
+
+
+//http request
+
+/*
+fetch(" https://jsonplaceholder.typicode.com/users "
+).then((result) => result.json()
+).then((data) => console.log(data)
+).then((error) => console.log(error));
+*/
+
+
+// !!!!!!!! ASYNC AWAIT !!!!!!!!!!1
+
+/*
+async function getData(){
+    try {
+    const result = await fetch(" https://jsonplaceholder.typicode.com/users ");
+    const data = await result.json();
+
+    console.log(data);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+getData();
+*/
+
+//const getData = async () => {};
+
+
